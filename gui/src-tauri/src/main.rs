44698e024ba86259d70d9a2984cde72543803a89
CarbonSmasher <crabonsmasher@gmail.com>
2025-11-04 10:54:44 -0500

    fix(gui): Hopefully fixes broken scrolling on Wayland

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -293,5 +293,6 @@ fn fix_compatability() {
 	// Fix for Wayland causing the app to crash
 	if std::env::var("WAYLAND_DISPLAY").is_ok() {
 		std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+		std::env::set_var("GTK_OVERLAY_SCROLLING", "0");
 	}
 }
