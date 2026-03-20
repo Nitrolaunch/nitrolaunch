fbb5a398aac8adcfabcaaed2cdd2489c2953bc02
CarbonSmasher <crabonsmasher@gmail.com>
2026-03-20 19:33:20 -0400

    refactor(gui): Move console logic to its own component

---
+++ gui/src/components/launch/Console.tsx
@@ -0,0 +1,220 @@
+import {
+	createMemo,
+	createSignal,
+	For,
+	Match,
+	Show,
+	Switch,
+} from "solid-js";
+import "./Console.css";
+import InlineSelect from "../input/select/InlineSelect";
+import SearchBar from "../input/text/SearchBar";
+import Icon from "../Icon";
+import { AngleDown, AngleRight, Error, Info, Text, Warning } from "../../icons";
+import Dropdown, { Option } from "../input/select/Dropdown";
+
+// Console for logs
+export default function Console(props: ConsoleProps) {
+	let outputElem!: HTMLDivElement;
+
+	let [filter, setFilter] = createSignal("all");
+	let [search, setSearch] = createSignal("");
+
+	let [input, setInput] = createSignal("");
+
+	let output = () => {
+		if (props.output == undefined) {
+			return undefined;
+		} else {
+			let lines = props.output.split("\n");
+
+			return lines;
+		}
+	};
+
+	function scrollToBottom() {
+		if (outputElem != undefined) {
+			outputElem.scrollTop = outputElem.scrollHeight;
+		}
+	}
+
+	return (
+		<div class="cont col instance-console">
+			<div class="cont fullwidth instance-console-header">
+				<InlineSelect
+					options={[
+						{
+							value: "all",
+							contents: (
+								<div class="cont">
+									<Icon icon={Text} size="1rem" />
+									ALL
+								</div>
+							),
+							tip: "Show all messages",
+						},
+						{
+							value: "error",
+							contents: (
+								<div class="cont">
+									<Icon icon={Error} size="1rem" />
+									ERRORS
+								</div>
+							),
+							color: "var(--error)",
+							tip: "Show only errors",
+						},
+						{
+							value: "warning",
+							contents: (
+								<div class="cont">
+									<Icon icon={Warning} size="1rem" />
+									WARNINGS
+								</div>
+							),
+							color: "var(--warning)",
+							tip: "Show only warnings",
+						},
+						{
+							value: "info",
+							contents: (
+								<div class="cont">
+									<Icon icon={Info} size="1rem" />
+									INFO
+								</div>
+							),
+							color: "var(--fg3)",
+							tip: "Show only info messages",
+						},
+					]}
+					selected={filter()}
+					onChange={setFilter}
+					connected={false}
+					columns={4}
+					solidSelect
+				/>
+				<div class="cont">
+					<div class="cont" style="width:14rem">
+						<Dropdown
+							options={[
+								{
+									value: undefined,
+									contents: "Current Output",
+								} as Option,
+							].concat(
+								props.availableLogs.map((x) => {
+									return {
+										value: x,
+										contents: x,
+									};
+								}),
+							)}
+							selected={props.selectedLog}
+							onChange={props.setSelectedLog}
+							zIndex="2"
+						/>
+					</div>
+				</div>
+				<div class="cont end fullwidth">
+					<SearchBar value={search()} method={setSearch} immediate />
+				</div>
+			</div>
+			<div class="cont col instance-console-output">
+				<Switch>
+					<Match when={output() != undefined}>
+						<div class="cont col instance-console-text" ref={outputElem}>
+							<For each={output()!}>
+								{(line) => {
+									let cls = line.includes("INFO")
+										? "info"
+										: line.includes("WARN")
+											? "warning"
+											: line.includes("ERROR")
+												? "error"
+												: "";
+
+									let isVisible = createMemo((input) => {
+										let filter2 = filter();
+										let search2 = search();
+
+										if (input == undefined) {
+											return true;
+										}
+
+										if (filter2 != "all" && filter2 != cls) {
+											return false;
+										}
+
+										if (
+											search2 != undefined &&
+											search2.length > 0 &&
+											!line
+												.toLocaleLowerCase()
+												.includes(search2.toLocaleLowerCase())
+										) {
+											return false;
+										}
+
+										return true;
+									});
+
+									return (
+										<span
+											class={`console-line ${cls}`}
+											style={`${isVisible() ? "" : "display:none"}`}
+										>
+											{line}
+										</span>
+									);
+								}}
+							</For>
+						</div>
+						<div
+							class="cont shadow bubble-hover instance-console-scroll"
+							onclick={() => {
+								scrollToBottom();
+							}}
+						>
+							<Icon icon={AngleDown} size="1.5rem" />
+						</div>
+						<Show when={props.sendMessage != undefined}>
+							<div class="fullwidth instance-console-input">
+								<form
+									onsubmit={async (e) => {
+										e.preventDefault();
+										props.sendMessage!(input() + "\n");
+										setInput("");
+									}}
+								>
+									<input
+										class="fullwidth"
+										value={input()}
+										onchange={(e) => setInput(e.target.value)}
+									/>
+								</form>
+								<div class="cont instance-console-input-prompt">
+									<Icon icon={AngleRight} size="1rem" />
+								</div>
+							</div>
+						</Show>
+					</Match>
+					<Match when={props.loadState == "errored"}>
+						Failed to load
+					</Match>
+					<Match when={props.loadState == "pending"}>Loading...</Match>
+					<Match when={props.loadState == "ready"}>No log found</Match>
+				</Switch>
+			</div>
+		</div>
+	);
+}
+
+export interface ConsoleProps {
+	loadState: "pending" | "ready" | "errored" | "unresolved" | "refreshing";
+	fetchOutput: (log: string | undefined) => void;
+	output?: string;
+	selectedLog?: string;
+	setSelectedLog: (log: string | undefined) => void;
+	availableLogs: string[];
+	sendMessage?: (message: string) => void;
+}
--- gui/src/components/launch/InstanceConsole.tsx
+++ gui/src/components/launch/InstanceConsole.tsx
@@ -2,33 +2,17 @@ import { invoke } from "@tauri-apps/api/core";
 import { Event, listen } from "@tauri-apps/api/event";
 import {
 	createEffect,
-	createMemo,
 	createResource,
 	createSignal,
-	For,
-	Match,
 	onCleanup,
-	Show,
-	Switch,
 } from "solid-js";
 import "./InstanceConsole.css";
-import InlineSelect from "../input/select/InlineSelect";
-import SearchBar from "../input/text/SearchBar";
-import Icon from "../Icon";
-import { AngleDown, AngleRight, Error, Info, Text, Warning } from "../../icons";
 import { errorToast } from "../dialog/Toasts";
-import Dropdown, { Option } from "../input/select/Dropdown";
+import Console from "./Console";
 
 export default function InstanceConsole(props: InstanceConsoleProps) {
-	let outputElem!: HTMLDivElement;
-
 	// Undefined for the current instance output
 	let [selectedLog, setSelectedLog] = createSignal<string | undefined>();
-
-	let [filter, setFilter] = createSignal("all");
-	let [search, setSearch] = createSignal("");
-
-	let [input, setInput] = createSignal("");
 
 	let [output, outputMethods] = createResource(
 		() => props.instanceId,
@@ -51,12 +35,7 @@ export default function InstanceConsole(props: InstanceConsoleProps) {
 					})) as string;
 				}
 
-				// Format the output into lines
-				let lines = text.split("\n");
-
-				scrollToBottom();
-
-				return lines;
+				return text;
 			} catch (e) {
 				console.error(e);
 			}
@@ -102,189 +81,26 @@ export default function InstanceConsole(props: InstanceConsoleProps) {
 		{ initialValue: [] },
 	);
 
-	function scrollToBottom() {
-		if (outputElem != undefined) {
-			outputElem.scrollTop = outputElem.scrollHeight;
+	let sendMessage = async (message: string) => {
+		try {
+			await invoke("write_instance_input", {
+				instanceId: props.instanceId,
+				input: message,
+			});
+		} catch (e) {
+			errorToast("Failed to send: " + e);
 		}
 	}
 
-	return (
-		<div class="cont col instance-console">
-			<div class="cont fullwidth instance-console-header">
-				<InlineSelect
-					options={[
-						{
-							value: "all",
-							contents: (
-								<div class="cont">
-									<Icon icon={Text} size="1rem" />
-									ALL
-								</div>
-							),
-							tip: "Show all messages",
-						},
-						{
-							value: "error",
-							contents: (
-								<div class="cont">
-									<Icon icon={Error} size="1rem" />
-									ERRORS
-								</div>
-							),
-							color: "var(--error)",
-							tip: "Show only errors",
-						},
-						{
-							value: "warning",
-							contents: (
-								<div class="cont">
-									<Icon icon={Warning} size="1rem" />
-									WARNINGS
-								</div>
-							),
-							color: "var(--warning)",
-							tip: "Show only warnings",
-						},
-						{
-							value: "info",
-							contents: (
-								<div class="cont">
-									<Icon icon={Info} size="1rem" />
-									INFO
-								</div>
-							),
-							color: "var(--fg3)",
-							tip: "Show only info messages",
-						},
-					]}
-					selected={filter()}
-					onChange={setFilter}
-					connected={false}
-					columns={4}
-					solidSelect
-				/>
-				<div class="cont">
-					<div class="cont" style="width:14rem">
-						<Dropdown
-							options={[
-								{
-									value: undefined,
-									contents: "Current Output",
-								} as Option,
-							].concat(
-								availableLogs().map((x) => {
-									return {
-										value: x,
-										contents: x,
-									};
-								}),
-							)}
-							selected={selectedLog()}
-							onChange={setSelectedLog}
-							zIndex="2"
-						/>
-					</div>
-				</div>
-				<div class="cont end fullwidth">
-					<SearchBar value={search()} method={setSearch} immediate />
-				</div>
-			</div>
-			<div class="cont col instance-console-output">
-				<Switch>
-					<Match when={output() != undefined}>
-						<div class="cont col instance-console-text" ref={outputElem}>
-							<For each={output()!}>
-								{(line) => {
-									let cls = line.includes("INFO")
-										? "info"
-										: line.includes("WARN")
-											? "warning"
-											: line.includes("ERROR")
-												? "error"
-												: "";
-
-									let isVisible = createMemo((input) => {
-										let filter2 = filter();
-										let search2 = search();
-
-										if (input == undefined) {
-											return true;
-										}
-
-										if (filter2 != "all" && filter2 != cls) {
-											return false;
-										}
-
-										if (
-											search2 != undefined &&
-											search2.length > 0 &&
-											!line
-												.toLocaleLowerCase()
-												.includes(search2.toLocaleLowerCase())
-										) {
-											return false;
-										}
-
-										return true;
-									});
-
-									return (
-										<span
-											class={`console-line ${cls}`}
-											style={`${isVisible() ? "" : "display:none"}`}
-										>
-											{line}
-										</span>
-									);
-								}}
-							</For>
-						</div>
-						<div
-							class="cont shadow bubble-hover instance-console-scroll"
-							onclick={() => {
-								scrollToBottom();
-							}}
-						>
-							<Icon icon={AngleDown} size="1.5rem" />
-						</div>
-						<Show when={props.isServer}>
-							<div class="fullwidth instance-console-input">
-								<form
-									onsubmit={async (e) => {
-										e.preventDefault();
-
-										try {
-											await invoke("write_instance_input", {
-												instanceId: props.instanceId,
-												input: input() + "\n",
-											});
-											setInput("");
-										} catch (e) {
-											errorToast("Failed to send: " + e);
-										}
-									}}
-								>
-									<input
-										class="fullwidth"
-										value={input()}
-										onchange={(e) => setInput(e.target.value)}
-									/>
-								</form>
-								<div class="cont instance-console-input-prompt">
-									<Icon icon={AngleRight} size="1rem" />
-								</div>
-							</div>
-						</Show>
-					</Match>
-					<Match when={output.state == "errored"}>
-						Failed to load: {output.error}
-					</Match>
-					<Match when={output.state == "pending"}>Loading...</Match>
-					<Match when={!output.loading}>No log found</Match>
-				</Switch>
-			</div>
-		</div>
-	);
+	return <Console
+		loadState={output.state}
+		output={output()}
+		fetchOutput={outputMethods.refetch}
+		sendMessage={props.isServer ? sendMessage : undefined}
+		availableLogs={availableLogs()}
+		selectedLog={selectedLog()}
+		setSelectedLog={setSelectedLog}
+	/>;
 }
 
 export interface InstanceConsoleProps {
