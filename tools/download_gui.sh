6ca5fcf8a29598bb22605bde52c0b881593004bb
CarbonSmasher <crabonsmasher@gmail.com>
2026-05-08 20:38:05 -0400

    feat: Update download script

--- tools/download_gui.sh
+++
@@ -1,3 +0,0 @@
-curl -L -o release/ubuntu-latest.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build_gui/main/ubuntu-latest.zip
-curl -L -o release/windows-2022.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build_gui/main/windows-2022.zip
-curl -L -o release/macos-latest.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build_gui/main/macos-latest.zip
