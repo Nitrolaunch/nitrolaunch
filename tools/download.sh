1e9641a083bfdb0f40a6334899bfd598355969d4
CarbonSmasher <crabonsmasher@gmail.com>
2026-08-10 00:34:27 -0400

    fix: Use correct branch

--- tools/download.sh
+++ tools/download.sh
@@ -1,7 +1,7 @@
-curl -L -o release/linux.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/dev/linux-x86_64.zip
-curl -L -o release/windows.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/dev/windows.zip
-curl -L -o release/macos.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/dev/macos-universal.zip
+curl -L -o release/linux.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/main/linux-x86_64.zip
+curl -L -o release/windows.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/main/windows.zip
+curl -L -o release/macos.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/gui/main/macos-universal.zip
 
-curl -L -o release/cli-linux.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/dev/ubuntu-latest.zip
-curl -L -o release/cli-windows.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/dev/windows-latest.zip
-curl -L -o release/cli-macos.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/dev/macos-latest.zip
+curl -L -o release/cli-linux.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/main/ubuntu-latest.zip
+curl -L -o release/cli-windows.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/main/windows-latest.zip
+curl -L -o release/cli-macos.zip https://nightly.link/Nitrolaunch/nitrolaunch/workflows/build/main/macos-latest.zip
