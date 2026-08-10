db303513ed3073448009b344c066dff9bf85ffb7
CarbonSmasher <crabonsmasher@gmail.com>
2026-08-10 09:59:49 -0400

    ci: Fix release script again

--- tools/release_gui.sh
+++ tools/release_gui.sh
@@ -15,14 +15,14 @@ unzip -d release/unzip release/windows.zip
 unzip -d release/unzip release/macos.zip
 
 # Linux
-mv "release/unzip/crates/gui/dist/nitro_gui_${VERSION}_x86_64.AppImage" "release/out/Nitrolaunch_gui_${VERSION}_x86_64.AppImage"
-mv "release/unzip/crates/gui/dist/nitro_gui_${VERSION}_amd64.deb" "release/out/Nitrolaunch_gui_${VERSION}_amd64.deb"
+mv "release/unzip/nitro_gui_${VERSION}_x86_64.AppImage" "release/out/Nitrolaunch_gui_${VERSION}_x86_64.AppImage"
+mv "release/unzip/nitro_gui_${VERSION}_amd64.deb" "release/out/Nitrolaunch_gui_${VERSION}_amd64.deb"
 
 # Windows
-mv "release/unzip/crates/gui/dist/nitro_gui_${VERSION}_x64_en-US.msi" "release/out/Nitrolaunch_gui_${VERSION}_x64_en-US.msi"
+mv "release/unzip/nitro_gui_${VERSION}_x64_en-US.msi" "release/out/Nitrolaunch_gui_${VERSION}_x64_en-US.msi"
 
 # MacOS
-mv "release/unzip/crates/gui/dist/Nitrolaunch_${VERSION}_aarch64.dmg" "release/out/Nitrolaunch_gui_${VERSION}_universal.dmg"
+mv "release/unzip/Nitrolaunch_${VERSION}_aarch64.dmg" "release/out/Nitrolaunch_gui_${VERSION}_universal.dmg"
 
 rm -r release/unzip
 
